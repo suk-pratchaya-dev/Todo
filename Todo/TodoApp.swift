1570ab4577ccab4423a6861b41e7557d0b6dac79
Pratchaya Suksena <ext.pratchaya.suk@truedigital.com>
2021-09-20 19:09:22 +0700

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Pratchaya Suksena on 20/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
